af5a585af88f9f023d7a344bec20f86421fc6cb8
uqgcb <leon.hanschmann@student.kit.edu>
2020-12-11 18:12:17 +0100

    update Filter and examples

--- Visualization/Filter.py
+++ Visualization/Filter.py
@@ -12,14 +12,14 @@ def filter_dataframe(dataframe, filterindex, aggregatevalues, aggregate, filterk
             if v[1] is dataframe.select_dtypes(include='object').to_dict():
                 v[1] = "\'" + v[1] + "\'" '''
     return pd.pivot_table(dataframe.query(" & ".join('{0} {1} {2}'.format(k, cond[0], cond[1]) for k, cond in filterkeywords.items())), index=["Country"], values=["Sales"], aggfunc={"Sales": "sum"})
+    #return pd.pivot_table(dataframe.query(" & ".join('{0} {1} {2}'.format(k, cond[0], "\'" + cond[1] + "\'" if dataframe.select_dtypes(include='object').to_dict().get(cond[1]), else: cond[1] ) for k, cond in filterkeywords.items())), index=["Country"], values=["Sales"], aggfunc={"Sales": "sum"})
 
     return pd.pivot_table(dataframe, index=[filterindex], values=[aggregatevalues], aggfunc={aggregatevalues: aggregate})
 
 
+#Test 
 working_dataframe = pd.read_csv(os.path.join(".", os.path.dirname(os.path.abspath(__file__)), "examples", "assets", "data", "FinancialSample.csv"))
-table_new = filter_dataframe(working_dataframe, "Country", "Sales", "mean", { "Product": ["==", "\'Carretera\'"], "Sales" : [">", "10000"], "Country": ["==", "\'Canada\'"]})
-
-
+table_new = filter_dataframe(working_dataframe, "Country", "Sales", "sum", { "Product": ["==", "\'Carretera\'"], "Sales": [">" , "10000"]})
 print(table_new.head())
 
 
