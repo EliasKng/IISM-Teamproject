cdbd456f02796f0a0018e7a62d66e32ae62013eb
Elias <elias.koenig@student.kit.edu>
2020-12-11 19:13:32 +0100

    Beautity code

--- Visualization/Filter.py
+++ Visualization/Filter.py
@@ -16,17 +16,15 @@ def prepare_string(xstr):
         except:
             return "\'"+xstr+"\'"
 
-def filter_dataframe(dataframe, filterindex, aggregatevalues, aggregate, filterkeywords): 
-    return pd.pivot_table(dataframe.query(" & ".join('{0} {1} {2}'.format(k, cond[0], prepare_string(cond[1])) for k, cond in filterkeywords.items())), index=["Country"], values=["Sales"], aggfunc={"Sales": "sum"})
-    #return pd.pivot_table(dataframe.query(" & ".join('{0} {1} {2}'.format(k, cond[0], "\'" + cond[1] + "\'" if dataframe.select_dtypes(include='object').to_dict().get(cond[1]), else: cond[1] ) for k, cond in filterkeywords.items())), index=["Country"], values=["Sales"], aggfunc={"Sales": "sum"})
-
-    #return pd.pivot_table(dataframe, index=[filterindex], values=[aggregatevalues], aggfunc={aggregatevalues: aggregate})
-
-print(prepare_string(x))
+#Takes a dataframe and filters for filterkeywords & aggregates by aggretae parameter (e.g. SUM)
+def prepare_dataframe(dataframe, filterindex, aggregatevalues, aggregate, filterkeywords): 
+    if filterkeywords :
+        return pd.pivot_table(dataframe.query(" & ".join('{0} {1} {2}'.format(k, cond[0], prepare_string(cond[1])) for k, cond in filterkeywords.items())), index=[filterindex], values=[aggregatevalues], aggfunc={aggregatevalues: aggregate})
+    return pd.pivot_table(dataframe, index=[filterindex], values=[aggregatevalues], aggfunc={aggregatevalues: aggregate})
 
 #Test 
 working_dataframe = pd.read_csv(os.path.join(".", os.path.dirname(os.path.abspath(__file__)), "examples", "assets", "data", "FinancialSample.csv"))
-table_new = filter_dataframe(working_dataframe, "Country", "Sales", "sum", { "Product": ["==", "Carretera"], "Sales": [">" , "10000"]})
+table_new = prepare_dataframe(working_dataframe, "Country", "Sales", "sum", { "Product": ["==", "Carretera"], "Sales": [">" , "10000"]})
 print(table_new.head())
 
 
