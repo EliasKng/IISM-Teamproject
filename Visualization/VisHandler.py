3f608772c90a4152f5ac794302fed89ecc99772c
uqgcb <leon.hanschmann@student.kit.edu>
2020-12-16 10:44:21 +0100

    Added Jsonify

--- Visualization/VisHandler.py
+++ Visualization/VisHandler.py
@@ -1,4 +1,5 @@
 import os
+import json
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -8,23 +9,36 @@ from PieChart import PieChart
 from ColumnChart import ColumnChart
 from ScatterPlot import ScatterPlot
 
+class VisHandler(): 
+    def __init__(self, vis_object): 
+        self.vis_object = vis_object
 
-def change_vistype(current_vis, target_vis): 
-    keywords = current_vis.keywords
 
-    #currentvis==BarChart 
-    if isinstance(current_vis, BarChart) and target_vis=="ColumnChart":
-        return ColumnChart(current_vis.dataframe, current_vis.get_y_encoding(), current_vis.get_x_encoding(), keywords)
-    elif isinstance(current_vis, BarChart) and target_vis=="PieChart":
-        return PieChart(current_vis.dataframe, current_vis.get_x_encoding(), current_vis.get_y_encoding(), keywords)
-    #currentvis==ColumnChart 
-    if isinstance(current_vis, ColumnChart) and target_vis=="BarChart":
-        return BarChart(current_vis.dataframe, current_vis.get_y_encoding(), current_vis.get_x_encoding(), keywords)
-    elif isinstance(current_vis, ColumnChart) and target_vis=="PieChart":
-        return  BarChart(current_vis.dataframe, current_vis.get_y_encoding(), current_vis.get_x_encoding(), keywords)
-    #currentvis==PieChart
-    if isinstance(current_vis, PieChart) and target_vis=="BarChart":
-        return BarChart(current_vis.dataframe, current_vis.get_color_encoding(), current_vis.get_theta_encoding(), keywords)
-    elif isinstance(current_vis, PieChart) and target_vis=="ColumnChart":
-        return BarChart(current_vis.dataframe, current_vis.get_theta_encoding(), current_vis.get_color_encoding(), keywords)
+    def change_vistype(self, current_vis, target_vis): 
+        keywords = current_vis.keywords
 
+        #currentvis==BarChart 
+        if isinstance(current_vis, BarChart) and target_vis=="ColumnChart":
+            self.vis_object = ColumnChart(current_vis.dataframe, current_vis.get_y_encoding(), current_vis.get_x_encoding(), keywords)
+            return self.vis_object
+        elif isinstance(current_vis, BarChart) and target_vis=="PieChart":
+            self.vis_object = PieChart(current_vis.dataframe, current_vis.get_x_encoding(), current_vis.get_y_encoding(), keywords)
+            return self.vis_object
+        #currentvis==ColumnChart 
+        if isinstance(current_vis, ColumnChart) and target_vis=="BarChart":
+            BarChart(current_vis.dataframe, current_vis.get_y_encoding(), current_vis.get_x_encoding(), keywords)
+            return self.vis_object
+        elif isinstance(current_vis, ColumnChart) and target_vis=="PieChart":
+            self.vis_object = BarChart(current_vis.dataframe, current_vis.get_y_encoding(), current_vis.get_x_encoding(), keywords)
+            return self.vis_object
+        #currentvis==PieChart
+        if isinstance(current_vis, PieChart) and target_vis=="BarChart":
+            self.vis_object = BarChart(current_vis.dataframe, current_vis.get_color_encoding(), current_vis.get_theta_encoding(), keywords)
+            return self.vis_object
+        elif isinstance(current_vis, PieChart) and target_vis=="ColumnChart":
+            self.vis_object = BarChart(current_vis.dataframe, current_vis.get_theta_encoding(), current_vis.get_color_encoding(), keywords)
+            return self.vis_object
+
+    #returns parsed JSON for frontend
+    def jsonify_vis(self): 
+        return json.dumps(json.loads(self.vis_object.dataframe.to_json(orient="split")), indent=4)
