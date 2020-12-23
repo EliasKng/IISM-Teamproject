080ebdc997f9751a89cafb4c32d06f1eaf341abe
uqgcb <leon.hanschmann@student.kit.edu>
2020-12-23 15:16:27 +0100

    added app.py

---
+++ app.py
@@ -0,0 +1,47 @@
+import os
+import sys
+
+sys.path.append(os.path.join(".", os.path.dirname(os.path.abspath(__file__)), "Visualization"))
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from Filter import prepare_dataframe, calculate_theta
+from BarChart import BarChart
+from ColumnChart import ColumnChart
+from PieChart import PieChart
+from ScatterPlot import ScatterPlot
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+from nl4dv import NL4DV
+from jsonPrinter import jsonPrettyPrinter
+
+
+
+#Import CSV Data
+working_dataframe = pd.read_csv(os.path.join(".", os.path.dirname(os.path.abspath(__file__)), "examples", "assets", "data", "FinancialSample.csv"))
+
+#NL4DV
+def nl4dvQueryAnalyzerFinancialsDataset(query) :
+    full_path = os.path.abspath(__file__)
+    nl4dv_instance = NL4DV(data_url = os.path.join(".", os.path.dirname(os.path.abspath(__file__)), "examples", "assets", "data", "FinancialSample.csv"))
+    dependency_parser_config = {"name": "corenlp", "model": os.path.join(".", os.path.dirname(os.path.abspath(__file__)), "examples","assets","jars","stanford-english-corenlp-2018-10-05-models.jar"),"parser": os.path.join(".", os.path.dirname(os.path.abspath(__file__)), "examples","assets","jars","stanford-parser.jar")}
+    nl4dv_instance.set_dependency_parser(config=dependency_parser_config)
+    queryInput = query
+    return nl4dv_instance.analyze_query(queryInput)
+
+
+# configuration
+DEBUG = True
+
+# instantiate the app
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+# enable CORS
+CORS(app, resources={r'/*': {'origins': '*'}})
+
+
+
+if __name__ == '__main__':
+    app.run()
