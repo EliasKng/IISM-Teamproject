743275bbf96ea2860a124b37d8335affe7f729ab
Vicky <vicky.zhang@web.de>
2021-01-04 17:43:07 +0100

    localhost:5000/data prints out message data, front and backend is connected

--- Dashboard-04012021/dashboard/src/components/Data.vue
+++ Dashboard-04012021/dashboard/src/components/Data.vue
@@ -5,12 +5,30 @@
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: 'Data',
   data() {
     return {
-      msg: 'DATA!',
+      msg: '',
     };
+  },
+  methods: {
+    getMessage() {
+      const path = 'http://localhost:5000/data';
+      axios.get(path)
+        .then((res) => {
+          this.msg = res.data;
+        })
+        .catch((error) => {
+          // eslint-disable-next-line
+          console.error(error);
+        });
+    },
+  },
+  created() {
+    this.getMessage();
   },
 };
 </script>
