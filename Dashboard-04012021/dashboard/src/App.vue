b605a037fea29a5f6d23b20abaea68f47b575aec
Vicky <vicky.zhang@web.de>
2021-01-05 13:41:03 +0100

    http://localhost:5000/data connected with backend works.

--- Dashboard-04012021/dashboard/src/App.vue
+++ Dashboard-04012021/dashboard/src/App.vue
@@ -4,44 +4,6 @@
   </div>
 </template>
 
-<script>
-import * as d3 from "d3";
-
-import {
-  Data,
-  Chart,
-} from '../src/';
-
-export default {
-  name: 'app',
-  data() {
-    return {
-      dataChart: data,
-      axisX: {
-        scale: 'scaleLinear',
-        range: [0, 400],
-        domain: [0, 10],
-        orient: 'Bottom',
-        width: 400,
-        height: 400
-      },
-      axisY: {
-        scale: 'scaleLinear',
-        range: [0, 400],
-        domain: [500, 0],
-        orient: 'Left',
-        width: 400,
-        height: 400
-      }
-    };
-  },
-  components: {
-    BarChart,
-    Data
-  },
-}
-</script>
-
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
