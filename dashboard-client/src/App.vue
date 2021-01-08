cef5d245d822cebf7ebc25b46eb2ae43599f3637
Niklas <udrfz@student.kit.edu>
2021-01-08 11:31:08 +0100

    Add Barchart

--- dashboard-client/src/App.vue
+++ dashboard-client/src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Home/>
+    <iframe src='https://webchat.botframework.com/embed/VisBot?s=hklRsvlqmDU.LlAinDiX1BF1692uQtyfVBMkEun7xtEe_smE_UvH6N4'  style='min-width: 400px; width: 100%; min-height: 500px;'></iframe>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Home
   }
 }
 </script>
