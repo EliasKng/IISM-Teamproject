ec6d2ec87efacadd05e951481a9859c7a780c886
Vicky <vicky.zhang@web.de>
2021-01-10 17:09:13 +0100

    hardcoded barchart

--- flask-vue-crud/dashboard-frontend/src/components/Visualization.vue
+++ flask-vue-crud/dashboard-frontend/src/components/Visualization.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
     <h1>Vis Bar Chart</h1>
+    <D3BarChart :config="barconfig" :datum="bardata"></D3BarChart>
     <p>{{ xAxis }}</p>
     <p>{{ yAxis }}</p>
     <p>{{ columns_output }}</p>
-    <D3BarChart :config="barconfig" :datum="bardata"></D3BarChart>
     <tbody>
         <tr v-for="(x, index) in data_output" :key="index">
             <td>{{ x }}</td>
@@ -16,6 +16,25 @@
 <script>
 import axios from 'axios';
 import { D3BarChart } from 'vue-d3-charts';
+/*
+function setBarChartConfig(xAxis, yAxis) {
+  return {
+    key: xAxis,
+    values: [yAxis],
+    color: { scheme: ['#41B882', '#222f3e'] },
+    transition: { ease: 'easeBounceOut', duration: 1000 },
+  };
+} */
+
+// [["Canada", 2894762.65],["France",6539875.15]]
+/* function setBarChartData() {
+  const z = [];
+  for (let i = 0; i < this.data_output.length; i += 1) {
+    const x = { Country: 'Canada', Sales: 12425 };
+    z.push(x);
+  }
+  return z;
+} */
 
 export default {
   name: 'Visualization',
@@ -23,6 +42,28 @@ export default {
     D3BarChart,
   },
   data() {
+    /* return {
+      bartext: '<D3BarChart :config="chart_config" :datum="chart_data"></D3BarChart>',
+      titlechart: '<D3BarChart title="Your custom title"></D3BarChart>',
+      sourcechart: '<D3BarChart source="Your custom source"></D3BarChart>',
+      barconfig: {
+        key: 'name',
+        values: ['total', 'balance'],
+        color: { scheme: ['#41B882', '#222f3e'] },
+        transition: { ease: 'easeBounceOut', duration: 1000 },
+      },
+      bardata: [
+        { name: '1992', total: 50000, balance: 748 },
+        { name: '1993', total: 555, balance: 5666 },
+        { name: '1994', total: 8574, balance: 574 },
+        { name: '1995', total: 15805, balance: 5805 },
+        { name: '1996', total: 14582, balance: 4582 },
+        { name: '1997', total: 26694, balance: 6694 },
+        { name: '1998', total: 35205, balance: 5205 },
+        { name: '1999', total: 45944, balance: 5944 },
+        { name: '2000', total: 78595, balance: 8595 },
+      ],
+    }; */
     return {
       columns_output: [],
       xAxis: '',
@@ -30,16 +71,19 @@ export default {
       bartext: '<D3BarChart :config="chart_config" :datum="chart_data"></D3BarChart>',
       titlechart: '<D3BarChart title="Your custom title"></D3BarChart>',
       sourcechart: '<D3BarChart source="Your custom source"></D3BarChart>',
+      /*       barconfig: setBarChartConfig('Country', 'Sales'),
+      bardata: setBarChartData(), */
+      // hardcoding works
       barconfig: {
-        key: this.xAxis,
-        values: [this.yAxis],
+        key: 'Country',
+        values: ['Sales'],
         color: { scheme: ['#41B882', '#222f3e'] },
         transition: { ease: 'easeBounceOut', duration: 1000 },
       },
       // [["Canada", 2894762.65],["France",6539875.15]]
       bardata: [
-        { Country: 'Canada', Sales: 50000 },
-        { Country: 'France', Sales: 555 },
+        { Country: 'Canada', Sales: 2894762.65 },
+        { Country: 'France', Sales: 6539875.15 },
       ],
     };
   },
