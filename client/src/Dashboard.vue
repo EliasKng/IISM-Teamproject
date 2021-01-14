695a55651eeca24c379bbce20211a6c2c9542e37
Niklas <udrfz@student.kit.edu>
2021-01-14 17:50:49 +0100

    add charts to dashboard

--- client/src/Dashboard.vue
+++ client/src/Dashboard.vue
@@ -1,52 +1,85 @@
 <template>
-    <div id="app">
-        <div class="grid__item">
-          <h1>Scatter Chart</h1>
-          <scatter-chart :data="dataChart" v-if="showScatterChart"/>
-        </div>
-        <div class="grid__item">
-          <h1>Bar Chart</h1>
-          <bar-chart :data="dataChart" width="500" height="500" v-if="showBarchart"/>
-        </div>
-        <div id="bot">
-            <iframe src='https://webchat.botframework.com/embed/VisBot?s=hklRsvlqmDU.LlAinDiX1BF1692uQtyfVBMkEun7xtEe_smE_UvH6N4'  style='width: 100%; height: 100%;'></iframe>
-        </div>
+  <div id="app">
+    <header id="header"><h1>IISM Teamprojekt Dashboard</h1></header>
+    <!-- eslint-disable-next-line -->
+    <!--v-if="charttype === 'Barchart'"-->
+    <D3BarChart :config="barconfig" :datum="barcolumndata" title="Lm" source="Dt" ></D3BarChart>
+    <!-- eslint-disable-next-line -->
+    <D3BarChart :config="columnconfig" :datum="barcolumndata" title="Lm" source="Dt" ></D3BarChart>
+    <!-- eslint-disable-next-line -->
+    <D3PieChart :config="pieconfig" :datum="piedata" title="Lo" source="Dl"></D3PieChart>
+    <h1>Scatter Chart</h1>
+        <scatter-chart :data="dataChart"/>
+    <div id="bot">
+        <iframe src='https://webchat.botframework.com/embed/VisBot?s=hklRsvlqmDU.LlAinDiX1BF1692uQtyfVBMkEun7xtEe_smE_UvH6N4'  style='width: 100%; height: 100%;'></iframe>
     </div>
+  </div>
 </template>
 
 <script>
+import { D3BarChart, D3PieChart } from 'vue-d3-charts';
 import ScatterChart from './components/ScatterChart.vue';
-import BarChart from './components/BarChart.vue';
 import { data } from './components/data';
+import { charttype } from './components/charttype';
 
 export default {
   name: 'app',
   data() {
     return {
-      showBarchart: false,
-      showScatterchart: true,
+      showchart: charttype,
       dataChart: data,
-      axisX: {
-        scale: 'scaleLinear',
-        range: [0, 400],
-        domain: [0, 10],
-        orient: 'Bottom',
-        width: 400,
-        height: 400,
+      barcolumndata: [
+        { name: '1992', total: 4748 },
+        { name: '1993', total: 5526 },
+        { name: '1994', total: 8574 },
+        { name: '1995', total: 15805 },
+        { name: '1996', total: 14582 },
+        { name: '1997', total: 26694 },
+        { name: '1998', total: 35205 },
+        { name: '1999', total: 45944 },
+        { name: '2000', total: 78595 },
+        { name: '2001', total: 78530 },
+        { name: '2002', total: 45407 },
+        { name: '2003', total: 54044 },
+        { name: '2004', total: 69165 },
+        { name: '2005', total: 61798 },
+        { name: '2006', total: 63686 },
+      ],
+      piedata: [
+        { name: 'Lorem', total: 30, color: '#425265' },
+        { name: 'Ipsum', total: 21, color: '#3e9a70' },
+        { name: 'Dolor', total: 20, color: '#41ab7b' },
+        { name: 'Sit', total: 112, color: '#222f3e' },
+      ],
+      barconfig: {
+        key: 'name',
+        values: ['total'],
+        orientation: 'vertical',
+        color: { current: '#41B882' },
+        axis: { yTitle: 'Lorem ipsum dolor sit amet', yTicks: 10, yFormat: '.0s' },
+        transition: { ease: 'easeBounceOut', duration: 1000 },
       },
-      axisY: {
-        scale: 'scaleLinear',
-        range: [0, 400],
-        domain: [500, 0],
-        orient: 'Left',
-        width: 400,
-        height: 400,
+      columnconfig: {
+        key: 'name',
+        values: ['total'],
+        orientation: 'horizontal',
+        color: { current: '#41B882' },
+        axis: { yTitle: 'Lorem ipsum dolor sit amet', yTicks: 10, yFormat: '.0s' },
+        transition: { ease: 'easeBounceOut', duration: 1000 },
+      },
+      pieconfig: {
+        key: 'name',
+        value: 'total',
+        radius: { inner: 80 },
+        color: { key: 'color' },
+        transition: { duration: 200 },
       },
     };
   },
   components: {
+    D3BarChart,
+    D3PieChart,
     ScatterChart,
-    BarChart,
   },
 };
 </script>
@@ -55,37 +88,35 @@ export default {
 body {
   margin: 0;
 }
-
+.header {
+/*  float:center;
+  font-weight: bold; */
+}
 .app {
-  margin-top: 20px;
+/*  margin-top: 20px;
   width: 1920px;
   height: 900px;
-  margin: 0 auto;
+  margin: 0 auto; */
 }
 
 .grid {
-  display: grid;
+/*  display: grid;
   grid-template-columns: repeat(2, 500px);
-  grid-gap: 20px;
+  grid-gap: 20px; */
 }
 
 #content {
+/*
   background-color: aqua;
   float: left;
   width: 98%;
-  height: 100%;
+  height: 100%; */
   }
 
-#diagramm {
-  background-color: rgb(255, 145, 0);
-  width: 1400px;
-  float: left;
-}
-
 #bot {
-  background-color: rgb(200, 255, 0);
+/*  background-color: rgb(200, 255, 0);
   width: 450px;
   float: right;
-  height: 900px;
+  height: 900px; */
 }
 </style>
