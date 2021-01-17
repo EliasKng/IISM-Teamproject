b5ca78590dea41cd7b77c1103659b6f7e91f67fb
uqgcb <leon.hanschmann@student.kit.edu>
2021-01-17 03:16:39 +0100

    fully connected Backend with frontend

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,6 +1,7 @@
 /* eslint-disable */
 
 
+import axios from 'axios';
 import Vue from 'vue';
 import Vuex from 'vuex';
 
@@ -11,15 +12,27 @@ export default new Vuex.Store({
     specs: {},
     type: "",
     columns: {},
-    data: 
-    [],
+    data: [],
   },
   mutations: {
     changeData(state, rawJSON) {
-      var objJSON = JSON.parse(rawJSON)
+      var objJSON = JSON.parse(rawJSON);
+      state.specs = {};
       state.data = objJSON["data"];
       state.type = objJSON["type"];
       state.columns = objJSON["columns"];
+    },
+    changeDataBot(state, rawJSON) {
+      var values = JSON.parse(rawJSON.values);
+      var specs = rawJSON.specs;
+      console.log(typeof values);
+      state.specs = specs;
+      console.log(specs);
+      console.log(typeof specs);
+      state.data = values["data"];
+      console.log(state.data);
+      state.type = values["type"];
+      state.columns = values["columns"];
     },
   },
   getters: {
@@ -42,16 +55,16 @@ export default new Vuex.Store({
     changeData(context, rawJSON) {
       context.commit('changeData', rawJSON);
     },
-    changeDataBot(context, Query, Endpoint) {
-      var url = 'http://127.0.0.1:5000' + Endpoint;
-      var json = JSON.stringify(Query);
-      axios(url, {
-        method: "post",
-        data: json,
-        withCredentials: true
-      })
-      .then(response => {context.commit('changeData', response.data)})
-  },
+    changeDataBot(context, backendSpecs) {
+      var value_obj = [backendSpecs["data"], backendSpecs["specs"]];
+      var endpoint = 'http://127.0.0.1:5000' + backendSpecs["endpoint"];
+      axios
+        .post(endpoint, value_obj, {headers: {"Content-Type": "application/json"}})
+        .then(response =>{
+          console.log((response.data));
+          context.commit('changeDataBot', (response.data))
+        })
+    },
   },
   modules: {
   },
